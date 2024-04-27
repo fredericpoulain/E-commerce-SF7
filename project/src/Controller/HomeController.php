1e84f3c76650029e43581cb119e26a86f79dfb14
Frédéric  Poulain <fredpoulain0210@gmail.com>
2024-04-27 17:39:57 +0200

    commit

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends AbstractController
     {
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'Fred',
+            'controller_name' => 'Bob',
         ]);
 
     }
