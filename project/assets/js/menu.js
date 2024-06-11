6b6fc2e681a3ab544447dc4343ad34b04114e75f
Frédéric Poulain <fredpoulain0210@gmail.com>
2024-06-11 15:34:04 +0200

    add mobile menu

--- project/assets/js/menu.js
+++ project/assets/js/menu.js
@@ -1,1 +1,21 @@
-console.log('menu.js !!')
+const allMenuCat = document.querySelectorAll('.menuCat');
+const allUlMenuCat = document.querySelectorAll('.ulMenuCat');
+
+allMenuCat.forEach(function (cat) {
+    cat.addEventListener('click', (e) => {
+        const nextElement = cat.nextElementSibling;
+            if (nextElement.classList.contains('dropdown')){
+                nextElement.classList.remove('dropdown')
+            }else{
+                allUlMenuCat.forEach(function (ul){
+                    ul.classList.remove('dropdown')
+                })
+                nextElement.classList.add('dropdown')
+            }
+
+
+
+    })
+});
+
+
